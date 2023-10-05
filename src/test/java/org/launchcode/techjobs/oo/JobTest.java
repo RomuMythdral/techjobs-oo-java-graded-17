928b4c8f598cad14e275c24389dd60ba0404f6d8
Hayden Kosko <h.kosko97@gmail.com>
2023-10-05 17:46:24 -0500

    Finished with task 4

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,39 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId () {
+    Job job1 = new Job();
+    Job job2 = new Job();
+    assertNotEquals(job1,job2);
+    }
+
+    @Test
+    public void  testJobConstructorSetsAllFields () {
+        Employer employer1 = new Employer("ACME");
+        Job job1 = new Job("Product tester", employer1, new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", employer1, new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job1.getName() instanceof String);
+        assertTrue(job1.getEmployer() instanceof Employer);
+        assertTrue(job1.getLocation() instanceof Location);
+        assertTrue(job1.getPositionType() instanceof PositionType);
+        assertTrue(job1.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals(job1.getName(), job2.getName());
+        assertEquals(job1.getEmployer(), job2.getEmployer());
+        assertEquals(job1.getLocation().toString(), job2.getLocation().toString());
+        assertEquals(job1.getPositionType().toString(), job2.getPositionType().toString());
+        assertEquals(job1.getCoreCompetency().toString(), job2.getCoreCompetency().toString());
+    }
+
+    @Test
+    public void testJobsForEquality () {
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(job1.getId() == job2.getId());
+    }
 }
